a956f851e60f5521e69c8b4814b4afb554655dec
Marcela Cardona <pineapplesoftwarecol@gmail.com>
2025-06-28 05:38:42 -0500

    correcciones de login y sing up intentando qeu funione

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 // Configuración desde variables de entorno
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function POST(req: NextRequest) {
